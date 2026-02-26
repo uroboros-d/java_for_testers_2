7cea1b674cb0f5cf61ea0c83d519dddbe2a09085
Dzmitry Shablinski <tassap2002@gmail.com>
2026-02-26 16:59:23 +0300

    в class GroupRemovalTests выделен isGroupPresent() (3.1)

--- addressbook_web_tests/src/test/java/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/GroupRemovalTests.java
@@ -6,11 +6,15 @@ public class GroupRemovalTests extends TestBase{
     @Test
     public void canRemoveGroup() {
         openGroupsPage();
-        if (! isElementPresent(By.name("selected[]"))) {
+        if (!isGroupPresent()) {
             createGroup( "", "", "");
         }
         removeGroup();
         driver.findElement(By.linkText("Logout")).click();
+    }
+
+    private boolean isGroupPresent() {
+        return isElementPresent(By.name("selected[]"));
     }
 
     private static void removeGroup() {
