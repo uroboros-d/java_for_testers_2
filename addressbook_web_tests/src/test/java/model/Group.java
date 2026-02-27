b1abc0dce714c79391b7481e872a786bc2f0c3b6
Dzmitry Shablinski <tassap2002@gmail.com>
2026-02-27 10:31:59 +0300

    озданы констр Group(), методы withName,withHeader,withFooter (3.2)

--- addressbook_web_tests/src/test/java/model/Group.java
+++ addressbook_web_tests/src/test/java/model/Group.java
@@ -1,4 +1,20 @@
 package model;
 
-public record Group(String group_name, String group_header, String group_footer) {
+public record Group(String name, String header, String footer) {
+
+    public Group() {
+        this( "", "", "");
+    }
+
+    public Group withName(String name) {
+        return new Group(name, this.header, this.footer);
+    }
+
+    public Group withHeader(String header) {
+        return new Group(this.name, header, this.footer);
+    }
+
+    public Group withFooter(String footer) {
+        return new Group(this.name, this.header, footer);
+    }
 }
