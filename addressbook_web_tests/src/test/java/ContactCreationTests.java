b949974864027b4c9e198030ee275e43aba5872f
Dzmitry Shablinski <tassap2002@gmail.com>
2026-02-27 11:23:30 +0300

    создан class ContactCreationTests c canCreateContact(),canCreateContactWithEmptyProperties() (задание 7)

---
+++ addressbook_web_tests/src/test/java/ContactCreationTests.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class ContactCreationTests extends TestBase {
+
+    @Test
+    public void canCreateContact() {
+        if (!isElementPresent(By.name("firstname"))) {
+            driver.findElement(By.linkText("add new")).click();
+        }
+        driver.findElement(By.name("firstname")).click();
+        driver.findElement(By.name("firstname")).sendKeys("firstname");
+        driver.findElement(By.name("lastname")).click();
+        driver.findElement(By.name("lastname")).sendKeys("lastname");
+        driver.findElement(By.name("address")).click();
+        driver.findElement(By.name("address")).sendKeys("address");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("home page")).click();
+    }
+
+    @Test
+    public void canCreateContactWithEmptyProperties() {
+        if (!isElementPresent(By.name("firstname"))) {
+            driver.findElement(By.linkText("add new")).click();
+        }
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("home page")).click();
+    }
+}
