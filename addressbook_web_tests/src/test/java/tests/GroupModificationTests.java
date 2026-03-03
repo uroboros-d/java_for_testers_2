09f7f5862e7c3f4bd8cea2173675c8703d439115
Dzmitry Shablinski <tassap2002@gmail.com>
2026-03-03 09:58:11 +0300

    создан class GroupModificationTests,метод canModifyGroup()(3.6)

---
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import model.Contact;
+import model.Group;
+import org.junit.jupiter.api.Test;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void canModifyGroup() {
+        if (!app.groups().isGroupPresent()) {
+            app.groups().createGroup(new Group());
+        }
+        app.groups().modifyGroup(new Group()
+                .withName("modified name")
+                .withHeader("modified header")
+                .withFooter("modified footer"));
+    }
+}
