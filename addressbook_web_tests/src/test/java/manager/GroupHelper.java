fffde8844a81ff3e9d8c05d9f86146d3ab876476
Dzmitry Shablinski <tassap2002@gmail.com>
2026-03-03 12:50:54 +0300

    создан GroupModificationTests и методы создания-удаления-изменения групп разбиты на мелкие м-ды(3.6)

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -24,22 +24,17 @@ public class GroupHelper {
 
     public void createGroup(Group group) {
         openGroupsPage();
-        manager.driver.findElement(By.name("new")).click();
-        manager.driver.findElement(By.name("group_name")).click();
-        manager.driver.findElement(By.name("group_name")).sendKeys(group.name());
-        manager.driver.findElement(By.name("group_header")).click();
-        manager.driver.findElement(By.name("group_header")).sendKeys(group.header());
-        manager.driver.findElement(By.name("group_footer")).click();
-        manager.driver.findElement(By.name("group_footer")).sendKeys(group.footer());
-        manager.driver.findElement(By.name("submit")).click();
-        manager.driver.findElement(By.linkText("group page")).click();
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupsPage();
     }
 
     public void removeGroup() {
         openGroupsPage();
-        manager.driver.findElement(By.name("selected[]")).click();
-        manager.driver.findElement(By.name("delete")).click();
-        manager.driver.findElement(By.linkText("group page")).click();
+        selectGroup();
+        submitGroupDeletion();
+        returnToGroupsPage();
     }
 
     public void modifyGroup(Group modifiedgroup) {
@@ -49,5 +44,42 @@ public class GroupHelper {
         fillGroupForm(modifiedgroup);
         submitGroupModification();
         returnToGroupsPage();
+    }
+
+    private void submitGroupCreation() {
+        manager.driver.findElement(By.name("submit")).click();
+    }
+
+    private void initGroupCreation() {
+        manager.driver.findElement(By.name("new")).click();
+    }
+
+    private void fillGroupForm(Group group) {
+        manager.driver.findElement(By.name("group_name")).click();
+        manager.driver.findElement(By.name("group_name")).sendKeys(group.name());
+        manager.driver.findElement(By.name("group_header")).click();
+        manager.driver.findElement(By.name("group_header")).sendKeys(group.header());
+        manager.driver.findElement(By.name("group_footer")).click();
+        manager.driver.findElement(By.name("group_footer")).sendKeys(group.footer());
+    }
+
+    private void submitGroupDeletion() {
+        manager.driver.findElement(By.name("delete")).click();
+    }
+
+    private void returnToGroupsPage() {
+        manager.driver.findElement(By.linkText("group page")).click();
+    }
+
+    private void selectGroup() {
+        manager.driver.findElement(By.name("selected[]")).click();
+    }
+
+    private void submitGroupModification() {
+        manager.driver.findElement(By.name("update")).click();
+    }
+
+    private void initGroupModification() {
+        manager.driver.findElement(By.name("update")).click();
     }
 }
