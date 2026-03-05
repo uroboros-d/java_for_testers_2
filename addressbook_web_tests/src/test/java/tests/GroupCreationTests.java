4fbe3d60710c65ed93ad207175ff8b41d8dd3994
Dzmitry Shablinski <tassap2002@gmail.com>
2026-03-05 17:21:27 +0300

    в canCreateMultipleGroups изменен пар-р на group => groupNameProvider изменен на groupProvider,закомент canCreateGroupWithEmptyName и canCreateGroupWithNameOnly()(4.5)

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -5,17 +5,23 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
-    public static List<String> groupNameProvider() {
-        var result = new ArrayList<>(List.of("name", "name'"));
+    public static List<Group> groupProvider() {
+        var result = new ArrayList<>(List.of(
+                new Group(),
+                new Group().withName("nameOnly"),
+                new Group().withName("name"),
+                new Group().withName("name'")));
         for (int i = 0; i < 5; i++) {
-            result.add(randomString(i * 10));
+            result.add(new Group()
+                    .withName(randomString(i * 10))
+                    .withHeader(randomString(i * 10))
+                    .withFooter(randomString(i * 10)));
         }
         return result;
     }
@@ -32,24 +38,21 @@ public class GroupCreationTests extends TestBase {
 //        Assertions.assertEquals(groupCount + 1, newGroupCount);
 //    }
 
-    @Test
-    public void canCreateGroupWithEmptyName() {
-        app.groups().createGroup(new Group());
-    }
+//    @Test
+//    public void canCreateGroupWithEmptyName() {
+//        app.groups().createGroup(new Group());
+//    }
 
-    @Test
-    public void canCreateGroupWithNameOnly() {
-        app.groups().createGroup(new Group().withName("nameOnly"));
-    }
+//    @Test
+//    public void canCreateGroupWithNameOnly() {
+//        app.groups().createGroup(new Group().withName("nameOnly"));
+//    }
 
     @ParameterizedTest
-    @MethodSource("groupNameProvider")
-    public void canCreateMultipleGroups(String name) {
+    @MethodSource("groupProvider")
+    public void canCreateMultipleGroups(Group group) {
         int groupCount = app.groups().getCount();
-        app.groups().createGroup(new Group()
-                    .withName(name)
-                    .withHeader("header")
-                    .withFooter("footer"));
+        app.groups().createGroup(group);
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount + 1, newGroupCount);
     }
