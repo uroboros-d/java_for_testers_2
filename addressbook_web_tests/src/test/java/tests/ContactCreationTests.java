84a54095cd37b4177914bf45db22124242cb5f10
Dzmitry Shablinski <tassap2002@gmail.com>
2026-03-05 12:42:53 +0300

    в canCreateContact() добавлена проверка кол-ва getCount()

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,16 +1,20 @@
 package tests;
 
 import model.Contact;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void canCreateContact() {
+        int contactCount = app.contacts().getCount();
         app.contacts().createContact(new Contact()
                 .withLastname("firstname")
                 .withFirstname("lastname")
                 .withAddress("address"));
+        int newContactCount = app.contacts().getCount();
+        Assertions.assertEquals(contactCount + 1, newContactCount);
     }
 
     @Test
