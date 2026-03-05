79e49b5a8feb11c5bfd3b0f6f21b7e851d499b05
Dzmitry Shablinski <tassap2002@gmail.com>
2026-03-05 12:47:41 +0300

    в canRemoveContact() проверка isContactPresent() заменена на кол-ва getCount()

--- addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
@@ -1,15 +1,19 @@
 package tests;
 
 import model.Contact;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ContactRemovalTests  extends TestBase {
 
         @Test
         public void canRemoveContact() {
-            if (!app.contacts().isContactPresent()) {
+            if (!app.contacts().getCount() == 0) {
                 app.contacts().createContact(new Contact());
             }
+            int contactCount = app.contacts().getCount();
             app.contacts().removeContact();
+            int newContactCount = app.contacts().getCount();
+            Assertions.assertEquals(contactCount - 1, newContactCount);
         }
 }
