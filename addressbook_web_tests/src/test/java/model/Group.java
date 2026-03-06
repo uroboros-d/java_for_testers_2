48ab13f5e453490b9d69463b9f0a1c652d87dd85
Dzmitry Shablinski <tassap2002@gmail.com>
2026-03-06 16:17:42 +0300

    в Group добавлен id(4.6)

--- addressbook_web_tests/src/test/java/model/Group.java
+++ addressbook_web_tests/src/test/java/model/Group.java
@@ -1,20 +1,20 @@
 package model;
 
-public record Group(String name, String header, String footer) {
+public record Group(String id, String name, String header, String footer) {
 
     public Group() {
-        this( "", "", "");
+        this("", "", "", "");
     }
 
     public Group withName(String name) {
-        return new Group(name, this.header, this.footer);
+        return new Group("", name, this.header, this.footer);
     }
 
     public Group withHeader(String header) {
-        return new Group(this.name, header, this.footer);
+        return new Group("", this.name, header, this.footer);
     }
 
     public Group withFooter(String footer) {
-        return new Group(this.name, this.header, footer);
+        return new Group("", this.name, this.header, footer);
     }
 }
