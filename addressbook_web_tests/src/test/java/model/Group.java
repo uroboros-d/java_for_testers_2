7a4c2991f123b0bcfe68634e25744dfeeebca927
Dzmitry Shablinski <tassap2002@gmail.com>
2026-03-09 10:08:23 +0300

    создан Group.withId,GroupHelper.getList(),переделан тест canRemoveGroup(4.6)

--- addressbook_web_tests/src/test/java/model/Group.java
+++ addressbook_web_tests/src/test/java/model/Group.java
@@ -3,18 +3,21 @@ package model;
 public record Group(String id, String name, String header, String footer) {
 
     public Group() {
-        this("", "", "", "");
+        this( "", "", "", "");
     }
 
+    public Group withId(String id) {
+        return new Group(id, this.name, this.header, this.footer);
+    }
     public Group withName(String name) {
-        return new Group("", name, this.header, this.footer);
+        return new Group(this.id, name, this.header, this.footer);
     }
 
     public Group withHeader(String header) {
-        return new Group("", this.name, header, this.footer);
+        return new Group(this.id, this.name, header, this.footer);
     }
 
     public Group withFooter(String footer) {
-        return new Group("", this.name, this.header, footer);
+        return new Group(this.id, this.name, this.header, footer);
     }
 }
--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -4,6 +4,10 @@ import model.Group;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GroupRemovalTests extends TestBase {
 
     @Test
@@ -11,10 +15,15 @@ public class GroupRemovalTests extends TestBase {
         if (app.groups().getCount() == 0) {
             app.groups().createGroup(new Group());
         }
-        int groupCount = app.groups().getCount();
-        app.groups().removeGroup();
-        int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount - 1, newGroupCount);
+        var oldGroups = app.groups().getList();
+        var rnd = new Random();
+        var index = rnd.nextInt(oldGroups.size());
+
+        app.groups().removeGroup(oldGroups.get(index));
+        var newGroups = app.groups().getList();
+        var expectedList = new ArrayList<>(oldGroups);
+        expectedList.remove(index);
+        Assertions.assertEquals(newGroups, expectedList);
     }
 
     @Test
