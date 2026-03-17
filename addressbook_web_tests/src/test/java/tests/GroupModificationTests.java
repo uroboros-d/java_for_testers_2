227998b3eee26aae6e35afe11369b8dbfe5ba668
Dzmitry Shablinski <tassap2002@gmail.com>
2026-03-17 11:52:27 +0300

    изменен GroupHelper.modifyGroup,в тест canModifyGroup добавлен компаратор(4.7)

--- addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -2,8 +2,11 @@ package tests;
 
 import model.Contact;
 import model.Group;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Random;
 
 public class GroupModificationTests extends TestBase {
@@ -16,9 +19,16 @@ public class GroupModificationTests extends TestBase {
         var oldGroups = app.groups().getList();
         var rnd = new Random();
         var index = rnd.nextInt(oldGroups.size());
-        app.groups().modifyGroup(oldGroups.get(index), new Group()
-                .withName("modified name")
-                .withHeader("modified header")
-                .withFooter("modified footer"));
+        var testData  = new Group().withName("modified name");
+        app.groups().modifyGroup(oldGroups.get(index), testData);
+        var newGroups = app.groups().getList();
+        var expectedList = new ArrayList<>(oldGroups);
+        expectedList.set(index, testData.withId(oldGroups.get(index).id()));
+        Comparator<Group> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newGroups.sort(compareById);
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newGroups, expectedList);
     }
 }
