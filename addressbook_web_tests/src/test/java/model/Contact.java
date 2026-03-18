fdd24dc739f7c5c7e03bcd164a0b0050870dc3ef
Dzmitry Shablinski <tassap2002@gmail.com>
2026-03-18 11:08:14 +0300

    в Contact добавлено свойство photo и withPhoto(5.1)

--- addressbook_web_tests/src/test/java/model/Contact.java
+++ addressbook_web_tests/src/test/java/model/Contact.java
@@ -1,24 +1,28 @@
 package model;
 
-public record Contact(String id, String lastname, String firstname, String address) {
+public record Contact(String id, String lastname, String firstname, String address, String photo) {
 
     public Contact() {
-        this( "", "", "", "");
+        this( "", "", "", "", "");
     }
 
     public Contact withId(String id) {
-        return new Contact(id, this.lastname, this.firstname, this.address);
+        return new Contact(id, this.lastname, this.firstname, this.address, this.photo);
     }
 
     public Contact withLastname(String lastname) {
-        return new Contact(this.id, lastname, this.firstname, this.address);
+        return new Contact(this.id, lastname, this.firstname, this.address, this.photo);
     }
 
     public Contact withFirstname(String firstname) {
-        return new Contact(this.id, this.lastname, firstname, this.address);
+        return new Contact(this.id, this.lastname, firstname, this.address, this.photo);
     }
 
     public Contact withAddress(String address) {
-        return new Contact(this.id, this.lastname, this.firstname, address);
+        return new Contact(this.id, this.lastname, this.firstname, address, this.photo);
+    }
+
+    public Contact withPhoto(String photo) {
+        return new Contact(this.id, this.lastname, this.firstname, this.address, photo);
     }
 }
