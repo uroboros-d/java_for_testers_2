244afe9a62fb191b4cd834d21d7be70833643bca
Dzmitry Shablinski <tassap2002@gmail.com>
2026-06-03 16:34:05 +0300

    создан DTO ContactRecord(6.2)

---
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/manager/hbm/ContactRecord.java
@@ -0,0 +1,21 @@
+package ru.stqa.addressbook.manager.hbm;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//вспомогательные классы DTO - data transfer object - описывают объекты соответствующие данным в Б
+@Entity
+@Table(name = "addressbook")
+public class ContactRecord {
+    @Id
+    @Column(name = "id")
+    public int id;
+    @Column(name = "firstname")
+    public String firstname;
+    @Column(name = "lastname")
+    public String lastname;
+    @Column(name = "address")
+    public String address;
+}
