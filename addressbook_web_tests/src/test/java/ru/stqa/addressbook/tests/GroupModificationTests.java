051cbee746f3ad33e15eb6111e4bf708a1d4ba92
Dzmitry Shablinski <tassap2002@gmail.com>
2026-06-05 10:54:39 +0300

    изменен GroupModificationTests.canModifyGroup().Всё работает (6.3)

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
@@ -12,15 +12,20 @@ public class GroupModificationTests extends TestBase {
 
     @Test
     public void canModifyGroup() {
-        if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new Group());
+//        if (app.groups().getCount() == 0) {
+//            app.groups().createGroup(new Group());
+//        }
+        if (app.hbm().getGroupCount() == 0) {
+            app.hbm().createGroup(new Group());
         }
-        var oldGroups = app.groups().getList();
+//        var oldGroups = app.groups().getList();
+        var oldGroups = app.hbm().getGroupList();
         var rnd = new Random();
         var index = rnd.nextInt(oldGroups.size());
         var testData  = new Group().withName("modified name");
         app.groups().modifyGroup(oldGroups.get(index), testData);
-        var newGroups = app.groups().getList();
+//        var newGroups = app.groups().getList();
+        var newGroups = app.hbm().getGroupList();
         var expectedList = new ArrayList<>(oldGroups);
         expectedList.set(index, testData.withId(oldGroups.get(index).id()));
         Comparator<Group> compareById = (o1, o2) -> {
